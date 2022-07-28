67e46be68cb1d96378a3240585a503eebc861842
Michał Mietła <mrbroomchannel@gmail.com>
2022-07-28 12:24:09 +0200

    Add buttons

--- js/script.js
+++ js/script.js
@@ -35,12 +35,33 @@ function displayResult(argPlayerMove, argComputerMove) {
     }
     printMessage('Zagrałem ' + argComputerMove + ', a Ty ' + argPlayerMove);
 }
-playerInput = prompt('Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce.');
-console.log('wybór ruchu gracza to: ' + playerInput);
-playerMove = getMoveName(playerInput);
-console.log('ruch gracza to: ' + playerMove);
-randomNumber = Math.floor(Math.random() * 3 + 1);
-console.log('wylosowana liczba to: ' + randomNumber);
-computerMove = getMoveName(randomNumber);
-console.log('ruch komputera to: ' + computerMove);
-displayResult(playerMove, computerMove);
+
+
+var argButtonName, buttonPaper, buttonRock, buttonScissors;
+
+/**
+ * Describe this function...
+ */
+function buttonClicked(argButtonName) {
+    clearMessages();
+    console.log(argButtonName + ' został kliknięty');
+    playerMove = argButtonName;
+    console.log('wybór ruchu gracza to: ' + playerInput);
+
+    console.log('ruch gracza to: ' + playerMove);
+    randomNumber = Math.floor(Math.random() * 3 + 1);
+    console.log('wylosowana liczba to: ' + randomNumber);
+    computerMove = getMoveName(randomNumber);
+    console.log('ruch komputera to: ' + computerMove);
+    displayResult(playerMove, computerMove);
+}
+
+
+
+
+buttonRock = document.getElementById('button-rock');
+buttonRock.addEventListener('click', function () { buttonClicked('kamień'); });
+buttonPaper = document.getElementById('button-paper');
+buttonPaper.addEventListener('click', function () { buttonClicked('papier'); });
+buttonScissors = document.getElementById('button-scissors');
+buttonScissors.addEventListener('click', function () { buttonClicked('nożyce'); });
